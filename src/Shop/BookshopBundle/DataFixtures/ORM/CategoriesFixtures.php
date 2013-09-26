d4a9206a757c83db98fc2901708ffaf7d54a01b0
Andrei Burian <aburian@pitechnologies.ro>
2013-09-26 17:30:33 +0300

    Fixed absolute path.

--- src/Shop/BookshopBundle/DataFixtures/ORM/CategoriesFixtures.php
+++ src/Shop/BookshopBundle/DataFixtures/ORM/CategoriesFixtures.php
@@ -7,18 +7,27 @@ use Nelmio\Alice\Fixtures;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Shop\BookshopBundle\Entity\Categories;
+use Symfony\Component\Finder\Finder;
 
 class CategoriesFixtures implements FixtureInterface {
-    
-    protected static $path = "/home/aburian/Sites/bravo/src/Shop/BookshopBundle/Resources/config/Fixtures/categories.yml";
-        
+
     public function load(ObjectManager $manager) {
+
+        $finder = new Finder();
+        $finder->files()->name('categories.yml')->in('src/Shop/BookshopBundle/Resources');
+
+        foreach ($finder as $file) {
+            // Print the absolute path
+            $path = $file->getRealpath();
+        }
+
         $loader = new \Nelmio\Alice\Loader\Yaml();
-        $objects = $loader->load(static::$path);
+        $objects = $loader->load($path);
 
         $persister = new \Nelmio\Alice\ORM\Doctrine($manager);
         $persister->persist($objects);
 
         $manager->flush();
     }
+
 }
