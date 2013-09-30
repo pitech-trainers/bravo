29556d7823747bc4d676c90cd8d55c70ea196399
Dan <dcoman@pitechplus.com>
2013-09-30 14:35:12 +0300

    Product page.

--- src/Shop/BookshopBundle/Controller/PageController.php
+++ src/Shop/BookshopBundle/Controller/PageController.php
@@ -15,6 +15,7 @@ class PageController extends Controller
 
     public function indexAction()
     {
+
         $em = $this->getDoctrine()
                 ->getManager();
 
@@ -27,29 +28,41 @@ class PageController extends Controller
 
     public function categoryAction()
     {
-        
+
         $request = $this->get('request');
 
         $em = $this->getDoctrine()
                 ->getManager();
-        
+
         $products = $em->getRepository('ShopBookshopBundle:Product')->getProducts($request->query->get('sortBy'),
-                                                                                  $request->query->get('direction'), 
-                                                                                  $request->query->get('cid'), 
-                                                                                  $request->query->get('st'), 
-                                                                                  $request->query->get('pr'), 
+                                                                                  $request->query->get('direction'),
+                                                                                  $request->query->get('cid'),
+                                                                                  $request->query->get('st'),
+                                                                                  $request->query->get('pr'),
                                                                                   $request->query->get('search'),
                                                                                   $request->query->get('cat'));
-        
+
         $helper = $this->get('helper');
         $cat = $helper->getCategoryForProducts($products);
-                
+
         $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
                 $products, $this->get('request')->query->get('page', 1)/* page number */, 3/* limit per page */
         );
-        
+
         return $this->render('ShopBookshopBundle:Page:category.html.twig', array('products' => $pagination, 'categories' => $cat));
     }
-    
+
+
+    public function productPageAction($id)
+    {
+        $em = $this->getDoctrine()
+                ->getManager();
+
+        $product = $em->getRepository('ShopBookshopBundle:Product')->find($id);
+        $randProd = $em->getRepository('ShopBookshopBundle:Product')->getRandomProd($product->getCategory()->getId(), 4);
+
+        return $this->render('ShopBookshopBundle:Page:product.html.twig', array("product" => $product, "randProd" => $randProd));
+    }
+
 }
